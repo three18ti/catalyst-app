fbf34b9aec707e3052d54b5fa1c05b76a439668d
three18ti <three18ti@gmail.com>
2013-02-21 13:51:31 -0700

    added base tables for server / owner

---
+++ lib/MyApp/Schema/Result/Owner.pm
@@ -0,0 +1,30 @@
+package MyApp::Schema::Result::Owner;
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'MyApp::Schema::Result';
+
+__PACKAGE__->table('role');
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'varchar',
+        size      => 16,
+        is_nullable => 0,
+        is_auto_increment => 1
+    },
+    name => {
+        data_type => 'varchar',
+        size      => 256,
+        is_nullable => 0,
+        is_auto_increment => 0,
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->has_many( servers => 'MyApp::Schema::Result::Server', 'id');
+
+__PACKAGE__->meta->make_immutable;
+1;
+
---
+++ lib/MyApp/Schema/Result/Server.pm
@@ -0,0 +1,30 @@
+package MyApp::Schema::Result::Server;
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'MyApp::Schema::Result';
+
+__PACKAGE__->table('server');
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'varchar',
+        size      => 16,
+        is_nullable => 0,
+        is_auto_increment => 1
+    },
+    name => {
+        data_type => 'varchar',
+        size      => 256,
+        is_nullable => 0,
+        is_auto_increment => 0,
+    },
+    
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->has_one( 'owner' => 'MyApp::Schema::Result::Owner', 'id' );
+
+__PACKAGE__->meta->make_immutable;
+1;
